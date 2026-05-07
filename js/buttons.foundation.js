c2ceb1eab0188deaca23f7dddb7d1a1c02ae3efe
Allan Jardine <allan@datatables.net>
2026-05-07 15:34:36 +0000

    Updating package for DataTables 3

--- js/buttons.foundation.js
+++ js/buttons.foundation.js
@@ -1,8 +1,56 @@
-/*! Foundation integration for DataTables' Buttons
- * © SpryMedia Ltd - datatables.net/license
+/*! Buttons Foundation styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-$.extend(true, DataTable.Buttons.defaults, {
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-zf', 'datatables.net-buttons'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-zf')(root);
+			}
+
+			if (! window.DataTable.Buttons) {
+				require('datatables.net-buttons')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+var Dom = DataTable.Dom;
+var util = DataTable.util;
+
+util.object.assignDeep(DataTable.Buttons.defaults, {
 	dom: {
 		container: {
 			tag: 'div',
@@ -48,14 +96,20 @@ $.extend(true, DataTable.Buttons.defaults, {
 	}
 });
 
-$(document).on('buttons-popover.dt', function () {
+Dom.s(document).on('buttons-popover.dt', function () {
 	var notButton = false;
-	$('.dtsp-panesContainer').each(function () {
-		if (!$(this).is('button')) {
+
+	Dom.s('.dtsp-panesContainer').each(function (el) {
+		if (!Dom.s(el).is('button')) {
 			notButton = true;
 		}
 	});
+
 	if (notButton) {
-		$('.dtsp-panesContainer').removeClass('button-group stacked');
+		Dom.s('.dtsp-panesContainer').classRemove('button-group stacked');
 	}
 });
+
+
+return DataTable;
+}));
